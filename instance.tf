af24e55c47653135ba4436570025fc9b5a4af408
Takuya Sugiyama (@motojouya) <motojouya@gmail.com>
2024-10-04 00:31:50 +0000

    copy from develop-ec2

---
+++ instance.tf
@@ -0,0 +1,34 @@
+resource "aws_instance" "develop" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  associate_public_ip_address = true
+  availability_zone           = var.availability_zone
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = ["${var.security_group_id}"]
+
+  key_name             = var.keypair_name
+  iam_instance_profile = var.profile_name
+
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      max_price = var.max_price
+    }
+  }
+
+  user_data = <<EOF
+#!/bin/bash
+curl https://raw.githubusercontent.com/motojouya/develop-ec2/main/resources/init.sh | bash -s -- ${var.region} ${var.ssh_port} ${var.volume_id} ${var.device_name} ${var.user_name} ${var.domain} ${var.hosted_zone_id}
+EOF
+
+  tags = {
+    Name = "develop"
+  }
+}
+
+resource "aws_volume_attachment" "develop_ebs_attachment" {
+  device_name = var.device_name
+  volume_id   = var.volume_id
+  instance_id = aws_instance.develop.id
+}
---
+++ variable.tf
@@ -0,0 +1,27 @@
+# network
+variable "region" {}
+variable "availability_zone" {}
+variable "subnet_id" {}
+variable "ssh_port" {}
+variable "security_group_id" {}
+
+# instance
+variable "instance_type" {}
+variable "max_price" {}
+variable "ami_id" {}
+# variable "tags" {
+#   type        = list(string)
+# }
+
+# domain
+variable "domain" {}
+variable "hosted_zone_id" {}
+
+# profiles
+variable "user_name" {}
+variable "keypair_name" {}
+variable "profile_name" {}
+
+# storage
+variable "device_name" {}
+variable "volume_id" {}
